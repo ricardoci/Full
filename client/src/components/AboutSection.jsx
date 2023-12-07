ecbc8d90a9768372766592aff95f8503ff121925
unknown <ricardocisneros197@gmail.com>
2023-12-07 10:26:15 -0800

    a

--- client/src/components/AboutSection.jsx
+++ client/src/components/AboutSection.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import Button from './Button';
 import PText from './PText';
 import SectionTitle from './SectionTitle';
-import AboutImg from '../assets/images/me.PNG';
+import AboutImg from '../assets/images/meSecond.JPEG';
 
 const AboutSectionStyles = styled.div`
   padding: 10rem 0;
